36b52f7c9a0357b802f813b87c12381765e93244
Kristijan Čeple <kristijan.ceple@gdi.net>
2023-07-09 23:39:23 +0200

    Delete redundant files

--- VendingMachinesSys/VendingMachinesSysWebApp/Models/RegisterViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VendingMachinesSysWebApp.Models
-{
-    public class RegisterViewModel
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-        // Add any additional properties as needed
-    }
-}
